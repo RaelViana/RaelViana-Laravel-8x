73c7019f0975a5b48dbd1e073f6170a1925b1564
RaelViana <catedralviana@hotmail.com>
2021-04-13 20:31:42 -0300

    Eloquent ORM no Laravel

---
+++ Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/** Comando cria um model com migrate integrado
+ * php artisan make:model Models\\Product -m 
+ */
+
+class Product extends Model
+{
+    // protected $table = 'products' ;
+}
